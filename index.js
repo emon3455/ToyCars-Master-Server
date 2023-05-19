727e604ab0bd32365b91d969289fbe03b28e7b6c
Emon <emon.mhk69@gmail.com>
2023-05-19 13:02:37 +0600

    delete toy done

--- index.js
+++ index.js
@@ -62,7 +62,12 @@ async function run() {
       res.send(result);
     });
 
-    
+    app.delete("/toys/:id", async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toysCollections.deleteOne(query);
+      res.send(result);
+    })
 
     
 
